7eb4ff547f66ec4e219d9cf83062358c94c70f4c
Farrukh <shahzadfarrukh621@gmail.com>
2024-06-04 01:26:39 +0530

    Added dummy data for food and travel and updated the tech blogs content as well

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import OurStory from './Pages/OurStory';
+import BlogDetail from './Pages/BlogDetail';
+import ContactUs from './Pages/ContactUs';
+import Write from './Pages/Write';
+import Profile from './Pages/Profile';
+import Home from './Pages/Home';
 
 function App() {
   return (
@@ -9,6 +16,16 @@ function App() {
     // </div>
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/OurStory' element={<OurStory/>}/>
+        <Route path='/Blogs/:id' element={<BlogDetail/>}/>
+        <Route path='/Contact-us' element={<ContactUs/>}/>
+        <Route path='/write' element={<Write/>}/>
+        <Route path='/Profile' element={<Profile/>}/>
+
+      </Routes>
       <Footer/>
     </div>
   );
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,32 @@
 import React, { useContext } from 'react'
-import { Theme } from '../Context/ThemeContext'
+import { Theme } from '../Context/ThemeContext';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+
 
 const HeroSection = () => {
     const {darkmode, toggleMode} = useContext(Theme);
+    const [text] = useTypewriter( {
+        words : ["Insight", "Stories","Inspiration"],
+        loop : 0,
+        typeSpeed : 100,
+        deleteSpeed : 60,
+        delaySpeed : 1000 
+    });
+
   return (
-    <div>
-        <div>
-            <h1>Wellcome to <span>TechPulse</span> , your daily dose of </h1>
+    <div className='bg-slate-700 min-h-200'>
+        <div className= 'ml-10 mt-15 h-150 bg-red-400'> 
+            <h1 className='text-xl'>    Welcome to 
+                    <span className='font-bold text-4xl'> TechPulse,</span> <br/>
+                    your daily dose of 
+                    <span className='font-bold text-3xl'> {text}</span>
+            </h1>
         </div>
+        
+            <button>
+                Get Started
+            </button>
+        
     </div>
   )
 }
